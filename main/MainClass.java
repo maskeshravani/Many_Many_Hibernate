674266e9458458c1c8cac8123d7610837e737918
maskeshravani <maskeshravani5@gmail.com>
2025-01-11 15:41:54 +0530

    Update

--- main/MainClass.java
+++ main/MainClass.java
@@ -1,56 +1,56 @@
 package com.main;
 
 import java.util.Scanner;
-
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+import com.entity.Cources;
+import com.entity.Student;
 
-import com.entity.Department;
-import com.entity.Employee;
+import java.util.ArrayList;
 
 public class MainClass {
 
-	public static void main(String[] args) {
-		Configuration cfg = new Configuration();
-		cfg.configure("hibernate.cfg.xml");
-		cfg.addAnnotatedClass(Department.class);
-		cfg.addAnnotatedClass(Employee.class);
+    public static void main(String[] args) {
+       
+        Configuration cfg = new Configuration();
+        cfg.configure("hibernate.cfg.xml");
+        cfg.addAnnotatedClass(Student.class);
+        cfg.addAnnotatedClass(Cources.class);
 
-		SessionFactory sf = cfg.buildSessionFactory();
-		Session ss = sf.openSession();
-		Transaction tr = ss.beginTransaction();
+        SessionFactory sf = cfg.buildSessionFactory();
+        Session ss = sf.openSession();
+        Transaction tr = ss.beginTransaction();
 
-		Scanner sc = new Scanner(System.in);
-		
-		Department d = new Department();
-		System.out.println("Enter the Id: ");
-		d.setD_id(sc.nextInt());
-		
-		System.out.println("Enter the deparment name: ");
-		d.setD_name(sc.next());
-		
-		d.setEmp(null);
-		
-		Employee e = new Employee();
-		System.out.println("Enter the Name:  ");
-		e.setE_name(sc.next());
-		
-		System.out.println("Enter the Name:  ");
-		e.setE_name(sc.next());
-		
-		System.out.println("Enter the Name:  ");
-		e.setE_name(sc.next());
-		
-		e.setDept(null);
-		
-		
-		tr.commit();
+        Scanner sc = new Scanner(System.in);
 
-		ss.close();
-		sf.close();
+        Cources c = new Cources();
+        System.out.println("Enter the Course Name: ");
+        c.setName(sc.next());
+        System.out.println("Enter the Duration: ");
+        c.setDuration(sc.next());
+        System.out.println("Enter the Fee: ");
+        c.setFee(sc.nextInt());
 
-	}
+        ss.persist(c);
+        
+        Student s = new Student();
+        System.out.println("Enter the Student Name: ");
+        s.setName(sc.next());
+        System.out.println("Enter the Email: ");
+        s.setEmail(sc.next());
 
+        c.getStudents().add(s); 
+        s.getCourses().add(c); 
+       
+        ss.persist(s);
+
+      
+        tr.commit();
+
+        ss.close();
+      
+        System.out.println("Data Saved Successfully!");
+    }
 }
---
+++ main/MainEmp.java
@@ -0,0 +1,56 @@
+package com.main;
+
+import java.util.Scanner;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import com.entity.Department;
+import com.entity.Employee;
+
+public class MainEmp {
+
+	public static void main(String[] args) {
+		Configuration cfg = new Configuration();
+		cfg.configure("hibernate.cfg.xml");
+		cfg.addAnnotatedClass(Department.class);
+		cfg.addAnnotatedClass(Employee.class);
+
+		SessionFactory sf = cfg.buildSessionFactory();
+		Session ss = sf.openSession();
+		Transaction tr = ss.beginTransaction();
+
+		Scanner sc = new Scanner(System.in);
+		
+		Department d = new Department();
+		System.out.println("Enter the Id: ");
+		d.setD_id(sc.nextInt());
+		
+		System.out.println("Enter the deparment name: ");
+		d.setD_name(sc.next());
+		
+		d.setEmp(null);
+		
+		Employee e = new Employee();
+		System.out.println("Enter the Name:  ");
+		e.setE_name(sc.next());
+		
+		System.out.println("Enter the Name:  ");
+		e.setE_name(sc.next());
+		
+		System.out.println("Enter the Name:  ");
+		e.setE_name(sc.next());
+		
+		e.setDept(null);
+		
+		
+		tr.commit();
+
+		ss.close();
+		sf.close();
+
+	}
+
+}
